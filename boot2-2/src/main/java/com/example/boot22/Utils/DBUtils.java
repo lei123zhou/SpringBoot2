c7bfdc4b46cacb00ec95368335387c15f35b7467
zl <675209316@qq.com>
2022-08-22 10:27:26 +0800

    tt

--- boot2-2/src/main/java/com/example/boot22/Utils/DBUtils.java
+++ boot2-2/src/main/java/com/example/boot22/Utils/DBUtils.java
@@ -7,20 +7,26 @@ import java.sql.SQLException;
 
 public class DBUtils {
     private static DruidDataSource dds;
-
     static {
+        //静态块里面的代码只执行一次
+        //创建数据库连接池
         dds = new DruidDataSource();
+        //设置数据库链接信息
         dds.setUrl("jdbc:mysql://localhost:3306/bootdb?" +
                 "characterEncoding=utf8&" +
                 "serverTimezone=Asia/Shanghai");
         dds.setUsername("root");
         dds.setPassword("520927zl");
-        dds.setInitialSize(3);
-        dds.setMaxActive(5);
+        dds.setInitialSize(3);//设置初始链接数量;
+        dds.setMaxActive(5);//设置最大连接数量
     }
 
-    public Connection getCon() throws SQLException {
+    //获取封装链接方法
+    public static Connection getCon() throws SQLException {
+
+        //从链接池中获取链接 异常抛出
         Connection conn = dds.getConnection();
+        System.out.println(conn);
         return conn;
     }
 }
--- boot2-2/src/main/java/com/example/boot22/cn/tedu/controller/UserController.java
+++ boot2-2/src/main/java/com/example/boot22/cn/tedu/controller/UserController.java
@@ -1,14 +1,55 @@
 package com.example.boot22.cn.tedu.controller;
 
+import com.example.boot22.Utils.DBUtils;
+import com.example.boot22.entity.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @Controller
 public class UserController {
     @RequestMapping("/reg")
     @ResponseBody
-    public String reg(){
+    public String reg(User user) {
+        try (Connection conn = DBUtils.getCon();) {
+            String sql = "insert into user values(null,?,?,?,?)";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1, user.getUsername());
+            ps.setString(2, user.getPassword());
+            ps.setString(3, user.getNick());
+            ps.setInt(4, user.getAge());
+            ps.executeUpdate();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return "注册成功";
+    }
 
+    @RequestMapping("/login")
+    @ResponseBody
+    public String login(User user) {
+        try (Connection conn = DBUtils.getCon();) {
+            String sql = "select password from user where username=?";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1, user.getUsername());
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                //如果rs.next存在下一个表示查到了这个账户对应的密码,下一步查验数据库里面的密码和用户输出的密码是否一样
+                String password = rs.getString(1);
+                if (password.equals(user.getPassword())) {
+                    return "登陆成功";
+                }
+                return "密码错误";
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        return "用户名不存在";
     }
 }
